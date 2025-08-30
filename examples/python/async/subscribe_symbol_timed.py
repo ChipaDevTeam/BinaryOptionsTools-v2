bfb1a0659496c2100288f2d1afacb0baa4c9432a
vigo walker <59869868+theshadow76@users.noreply.github.com>
2025-08-30 13:14:10 -0400

    Add delay before subscribing to symbol stream

--- examples/python/async/subscribe_symbol_timed.py
+++ examples/python/async/subscribe_symbol_timed.py
@@ -1,7 +1,7 @@
 from BinaryOptionsToolsV2.pocketoption import PocketOptionAsync
 from BinaryOptionsToolsV2.tracing import start_logs
 from datetime import timedelta
-
+import time
 import asyncio
 
 # Main part of the code
@@ -9,6 +9,7 @@ async def main(ssid: str):
     # The api automatically detects if the 'ssid' is for real or demo account
     start_logs(".", "INFO")
     api = PocketOptionAsync(ssid)    
+    time.sleep(5)
     stream = await api.subscribe_symbol_timed("EURUSD_otc", timedelta(seconds=5)) # Returns a candle obtained from combining candles that are inside a specific time range
     
     # This should run forever so you will need to force close the program
